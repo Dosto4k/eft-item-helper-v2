dcf09107323170e8abd708b4496bba39be7355a8
Dosto4k <dudcanos@yandex.ru>
2026-06-20 17:40:42 +0300

    feat(auth): add register user and jwt authentication

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,8 +1,8 @@
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("auth/", include("authentication.urls", namespace="authentication")),
 ] + debug_toolbar_urls()
